f72ce0c3d69df4f5dd7dd232154b141e1789471f
kasiCzka18 <164771943+kasiCzka18@users.noreply.github.com>
2026-01-29 22:29:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,36 +3,59 @@
 #include "Soldier.h"
 #include "Army.h"
 
+#include <iostream>>
 #include <QCoreApplication>
 //#include <random>
 
-Soldier newSoldier(UnitType type) {
-    SoldierBuilder builder;
-    if (type == UnitType::Archer)
-        return builder.setType(type).setHp(60).setDps(15).build();
-    if (type == UnitType::Cavalry)
-        return builder.setType(type).setHp(120).setDps(25).build();
-    return builder.setType(type).setHp(100).setDps(20).build();
-}
+//Soldier newSoldier(UnitType type)
+//{
+//    SoldierBuilder builder;
+//    if (type == UnitType::Archer)
+//        return builder.setType(type).setHp(60).setDps(15).build();
+//    if (type == UnitType::Cavalry)
+//        return builder.setType(type).setHp(120).setDps(25).build();
+//    return builder.setType(type).setHp(100).setDps(20).build();
+//}
 
-void generateArmy(Army& army) {
-    for (int i = 0; i < 100; ++i) {
-        army.archers.add(newSoldier(UnitType::Archer));
-        army.cavalry.add(newSoldier(UnitType::Cavalry));
-        army.infantry.add(newSoldier(UnitType::Infantry));
+void generateArmy(ComplexUnit& army)
+{
+    Director director;
+    SoldierBuilder soldierBuilder;
+    ComplexUnit *archers = new ComplexUnit;
+    ComplexUnit *infantry = new ComplexUnit;
+    ComplexUnit *cavalry = new ComplexUnit;
+
+    for (int i = 0; i < 100; ++i)
+    {
+        director.makeArcher(&soldierBuilder);
+        archers->add(soldierBuilder.getSoldier());
+
+        director.makeInfantry(&soldierBuilder);
+        infantry->add(soldierBuilder.getSoldier());
+
+        director.makeCavalry(&soldierBuilder);
+        cavalry->add(soldierBuilder.getSoldier());
     }
+    army.add(archers);
+    army.add(infantry);
+    army.add(cavalry);
 }
 
-int main() {
-    Army armyA, armyB;
+int main()
+{
+    ComplexUnit army1;
+    ComplexUnit army2;
 
-    generateArmy(armyA);
-    generateArmy(armyB);
+    generateArmy(army1);
+    generateArmy(army2);
 
-    std::cout << "Army A start: " << armyA.totalAlive() << "\n";
-    std::cout << "Army B start: " << armyB.totalAlive() << "\n";
+    std::cout << "Army A start: " << army1.getAliveCount() << "\n";
+    std::cout << "Army B start: " << army2.getAliveCount() << "\n";
 
-    ClassicBattleStrategy strategy;
-    BattleSimulator simulator(&strategy);
-    simulator.fight(armyA, armyB);
+    const auto &s = army2.getSoldiers();
+
+    std::cout << s.size();
+   ClassicBattleStrategy strategy;
+   BattleSimulator simulator(&strategy);
+   simulator.fight(army1, army2);
 }
